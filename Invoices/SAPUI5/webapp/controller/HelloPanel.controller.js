a7f3ce73ea7a2c694fb19c25c4a81c0c03f4c64f
jppalominos <juan.palominos@gmail.com>
2021-05-11 01:01:35 +0000

    Laboratorio 4.3 - Diálogos Definición

--- Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
+++ Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -1,13 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.m.MessageToast} MessageToast
+     * * @param {typeof sap.ui.core.Fragment} Fragment
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Fragment) {
         "use strict";
 
         return Controller.extend("logaligroup.SAPUI5.controller.HelloPanel", {
@@ -26,6 +28,21 @@ sap.ui.define([
                 var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
                 MessageToast.show(sMsg);
+            },
+
+            onOpenDialog: function () {
+
+                const oView = this.getView();
+
+                if (!this.byId("helloDialog")) {
+                    Fragment.load({
+                        id: oView.getId(),
+                        name: "logaligroup.SAPUI5.view.HelloDialog"
+                    }).then(function (oDialog) {
+                        oView.addDependent(oDialog);
+                        oDialog.open();
+                    });
+                }
             }
         });
     });
